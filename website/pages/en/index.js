df7bb4efb17a896263dcf8d574b4a400c0d448a7
Narayanan Krishnamurthy <narayanan.kmu@gmail.com>
2020-07-07 07:53:14 +0530

    try out

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -136,7 +136,7 @@ class Index extends React.Component {
         {[
           {
             content:
-              'Each new Docusaurus project has **randomly-generated** theme colors.',
+              'Our **Top** Videos.',
             image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
             imageAlign: 'right',
             title: 'Randomly Generated Theme Colors',
